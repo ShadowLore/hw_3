ac534d1926fb8a14acb40a33b00adb85bcd6255a
nfsri <nfsrivals757@gmail.com>
2020-12-02 16:30:51 +0300

    lol

--- ito.py
+++ ito.py
@@ -1,5 +1,5 @@
 homework_list = ['Пропылесосить', 'Помыть полы', 'Погладить белье', 'Приготовить ужин', 'Повесить полку', 'Отдохнуть', 'Помочь с уроками']
-#                   0        1         2       3          4       5       6
+#                      0                 1                 2                 3                   4              5             6
 
 print(homework_list[2])
 
@@ -9,12 +9,16 @@ print(homework_list[-1])
 print(homework_list[-4])
 print(homework_list[len(homework_list) - 4])
 
-homework_list.append('Петр')
+homework_list.append('Почистить обувь')
 print(homework_list)
 
-homework_list.insert(2, 'Роман')
+homework_list.insert(6, 'Поиграть с котом')
 print(homework_list)
 
 last_work = homework_list.pop()
 print(last_work)
 print(homework_list)
+
+print(homework_list.count('Отдохнуть'))
+
+print(homework_list.index('Отдохнуть'))
